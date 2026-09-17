bdfdf57a1a832f798a6f048397ef81f04ec228a8
zeina-benton <zeina.benton@ucf.edu>
2026-09-17 16:17:56 -0400

    hw4 q1,2,3

--- AST5765/Homework/hw4_zeinabenton/hw4_zeinabenton.py
+++ AST5765/Homework/hw4_zeinabenton/hw4_zeinabenton.py
@@ -3,7 +3,11 @@
 #9/17/26
 
 #%%
-from hw4_zeinabenton_support_functions import gaussian_random_generator
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.pyplot as plt 
+from hw4_zeinabenton_support_functions import gaussian_random_generator, gaussian
 
 print ("Question 2: The Gaussian distribution in Python")
 
@@ -14,4 +18,35 @@ sigma = 13
 
 gaussian_sample = gaussian_random_generator(N, mu, sigma)
 print("The Gaussian sample for part a is: ", gaussian_sample)
-# %%
+
+#b
+plt.figure(figsize=(10, 6))
+plt.hist(gaussian_sample, bins = np.arange(0, 101, 1), alpha=0.7, color='blue')
+plt.xlabel('x')
+plt.ylabel('N(x)')
+plt.title('Histogram of Gaussian Sample (N=10,000, mu=55, sigma=13)')
+plt.show()
+
+plt.savefig('hw4_zeinabenton_problem2b_plot1.png')
+
+#c
+x = np.arange(0.5, 100.5, 1) #center of each bin
+
+plt.figure(figsize=(10, 6))
+plt.hist(gaussian_sample, bins = np.arange(0, 101, 1), alpha=0.7, color='blue')
+plt.plot(x, N * gaussian(x, mu, sigma), color='red', linewidth=2, label='Gaussian PDF') #new addition
+plt.xlabel('x')
+plt.ylabel('N(x)')
+plt.title('Histogram of Gaussian Sample (N=10,000, mu=55, sigma=13) with Gaussian PDF')
+plt.show()
+
+plt.savefig('hw4_zeinabenton_problem2c_plot2.png')
+
+
+
+#---------------------------------
+
+
+
+
+
--- AST5765/Homework/hw4_zeinabenton/hw4_zeinabenton_support_functions.py
+++ AST5765/Homework/hw4_zeinabenton/hw4_zeinabenton_support_functions.py
@@ -1,9 +1,11 @@
-import numpy as np
-import scipy as sp
-import matplotlib as mpl
-import matplotlib.pyplot as plt  # NOTE: pyplot, *not* pylab!
-import astropy.io.fits as fits
+#Zeina Benton
+#HW4
+#9/17/26
 
+
+import numpy as np
+
+#2a
 def gaussian_random_generator(N, mu, sigma):
     """
     Generates N random numbers from a Gaussian distribution with mean mu and standard deviation sigma.
@@ -31,7 +33,7 @@ def gaussian_random_generator(N, mu, sigma):
     """ 
     return np.random.normal(mu, sigma, N)
 
-
+#2c
 def gaussian(x, mu, sigma):
     """
     Calculates the Gaussian probability density at each point in N.
@@ -67,4 +69,57 @@ def gaussian(x, mu, sigma):
     p_x = variables * exp
 
     return p_x
+
+
+#3a
+def fwhm(sigma):
+    """
+    Calculates the full width at half maximum (FWHM) of a Gaussian distribution.
+
+    -----
+    Parameters
+    ----------
+    sigma : float
+        The standard deviation of the Gaussian distribution.
+    Returns
+    -------
+    fwhm_value : float
+        The full width at half maximum (FWHM) of the Gaussian distribution.
+    Examples
+    -----
+    >>> print(fwhm(1))
+    2.3548200450309493
+
+    >>> print(fwhm(2))
+    4.7096400900618985
+
+    Notes
+    -----
+    Derivation:
+
+    p(x) = A * exp[-0.5 * ((x - mu)/sigma)^2]
+
+    A/2 = A * exp[-0.5 * ((x - mu)/sigma)^2]
+
+    1/2 = exp[-0.5 * ((x - mu)/sigma)^2]
+
+    ln(1/2) = -0.5 * ((x - mu)/sigma)^2
+
+    ln(2) = 0.5 * ((x - mu)/sigma)^2
+
+    2 * ln(2) = ((x - mu)/sigma)^2
+
+    sqrt(2 * ln(2)) = (x - mu)/sigma
+
+    x - mu = sigma * sqrt(2 * ln(2))
+
+    mu - sigma*sqrt(2ln(2))
+    mu + sigma*sqrt(2ln(2))
+
+    Therefore:
+    FWHM = 2*sigma*sqrt(2ln(2))
+
+    """
+    fwhm_value = 2 * np.sqrt(2 * np.log(2)) * sigma
+    return fwhm_value
 
